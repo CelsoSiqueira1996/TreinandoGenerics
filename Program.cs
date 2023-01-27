f1a79f6f80c0bb5a9313d507882431d5c19c406d
Celso <celso.jacinto.siqueira.neto@gmail.com>
2023-01-26 23:14:41 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿
+public class TreinandoGenerics
+{
+    static void Main(string[] args)
+    {
+        var teste1 = new Pessoa()
+        {
+            Nome = "Celso",
+            Idade = 26
+        };
+
+        var teste2 = new Pessoa()
+        {
+            Nome = "Celso",
+            Idade = 26
+        };
+
+        var teste3 = teste1;
+
+        Console.WriteLine(ComparandoTipos.Comparar(teste2, teste1));
+        Console.WriteLine(ComparandoTipos.Comparar(teste2, teste3));
+        Console.WriteLine(ComparandoTipos.Comparar(teste1, teste3));
+        Console.WriteLine(ComparandoTipos.Comparar(teste2.Nome, teste1.Nome));
+        Console.WriteLine(ComparandoTipos.Comparar(teste2.Idade, teste1.Idade));
+    }
+}
+public static class ComparandoTipos
+{
+    public static bool Comparar(string value1, string value2)
+    {
+        return value1 == value2;
+    }
+
+    public static bool Comparar(int value1, int value2)
+    {
+        return value1 == value2;
+    }
+
+    public static bool Comparar(double value1, double value2)
+    {
+        return value1 == value2;
+    }
+
+    public static bool Comparar<T>(T value1, T value2)
+    {
+        return value1.Equals(value2);
+    }
+}
+
+public class Pessoa
+{
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+}
+
